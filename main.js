7c0cfabc484909ca9ac35504b762162bc93b4bdc
Samyak Jain <samyakjain3098@gmail.com>
2018-10-02 17:23:42 +0530

    random color added

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 var button=document.querySelector("button");
 var filled=false;
 button.addEventListener("click",function(){
 	//alert('connected');
-	if(filled)
-		document.body.style.background="blue";
-	else
-		document.body.style.background="green";
-	filled=!filled;
+	// if(filled)
+	// 	document.body.style.background="blue";
+	// else
+	// 	document.body.style.background="green";
+	// filled=!filled;
+
+	document.body.style.background = getRandomColor();
+
+	// Shorter way to set a random color
+	// document.body.style.background = "#"+((1<<24)*Math.random()|0).toString(16);
 });
 
