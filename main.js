f7eb56cc0db94dc681f7bc2841404ebbbc375628
Harshita Gupta <66556662+harshitaaaaaa@users.noreply.github.com>
2020-10-04 22:01:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ var filled=false;
 button.addEventListener("click",function(){
 	//alert('connected');
 	if(filled)
-		document.body.style.background="blue";
+		document.body.style.background="#faf0ca";
 	else
-		document.body.style.background="green";
+		document.body.style.background="#c7f9cc";
 	filled=!filled;
 });
 
